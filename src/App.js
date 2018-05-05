aefb8514cf8707f4b2bb3a25e57bbd769949e8c3
Joe Pfahl <joegravity@gmail.com>
2018-05-05 15:37:40 -0500

    App working

--- src/App.js
+++ src/App.js
@@ -5,63 +5,90 @@ import Title from "./components/Title";
 import friends from "./friends.json";
 import "./App.css";
 
-// function shuffleFriends(array) {
-//   for (let i = array.length - 1; i > 0; i--) {
-//     let j = Math.floor(Math.random() * (i + 1));
-//     [array[i], array[j]] = [array[j], array[i]];
-//   }
-//   return array;
-// };
-
-function shuffleFriends(array) {
-  array.sort(function() { return 0.5 - Math.random() });
+function scramble(array) {
+  array.sort(function () { return 0.5 - Math.random() });
   return array;
 };
 
 class App extends Component {
   // Setting this.state.friends to the friends json array
   state = {
-    friends
+    friends,
+    currentScore: 0,
+    // topScore: 0,
+    // rightWrong: "",
+    clicked: [],
   };
 
   handleClick = id => {
-    // if (this.state.clicked.indexOf(id) === -1) {
-    //   this.handleIncrement();
-    //   this.setState({ clicked: this.state.clicked.concat(id) });
-    // } else {
-    //   this.handleReset();
-    // }
-    this.handleShuffle();
-
+    if (this.state.clicked.indexOf(id) === -1) {
+      this.handleIncrement();
+      this.setState({ clicked: this.state.clicked.concat(id) });
+    } else {
+      this.handleReset();
+    }
   };
 
-  // removeFriend = id => {
-  //   // Filter this.state.friends for friends with an id not equal to the id being removed
-  //   const friends = this.state.friends.filter(friend => friend.id !== id);
-  //   // Set this.state.friends equal to the new friends array
-  //   this.setState({ friends });
-  // };
+  handleIncrement = () => {
+    const newScore = this.state.currentScore + 1;
+    this.setState({
+      currentScore: newScore,
+      // rightWrong: ""
+    });
+    // if (newScore >= this.state.topScore) {
+    //   this.setState({ topScore: newScore });
+    // }
+    // else if (newScore === 12) {
+    if (newScore === 12) {
+      // this.setState({ rightWrong: "You win!" });
+      alert("You win!");
+    }
+    this.handleShuffle();
+  };
+
+  handleReset = () => {
+    this.setState({
+      currentScore: 0,
+      // topScore: this.state.topScore,
+      // rightWrong: "Glaven!",
+      clicked: []
+    });
+    alert("You Lose");
+    this.handleShuffle();
+  };
 
   handleShuffle = () => {
-    let shuffledFriends = shuffleFriends(friends);
-    this.setState({ friends: shuffledFriends });
+    let shuffled = scramble(friends);
+    this.setState({ friends: shuffled });
   };
 
   // Map over this.state.friends and render a FriendCard component for each friend object
   render() {
     return (
       <Wrapper>
-        <Title>Friends List</Title>
-        {this.state.friends.map(friend => (
-          <FriendCard
-            // removeFriend={this.removeFriend}
-            id={friend.id}
-            key={friend.id}
-            image={friend.image}
-            handleClick={this.handleClick}
-            handleShuffle={this.handleShuffle}
+        <div className="row">
+          <div className="col-6 col-md-5 bg-primary">
+            <h1 className="display-4">Clickety Clack Game</h1>
+          </div>
+          <Title
+            score={this.state.currentScore}
           />
-        ))}
+        </div>
+        <div className="row">
+          <h2>Try to click on each character, but don't hit any duplicates!</h2>
+        </div>
+        <div className="row">
+          {this.state.friends.map(friend => (
+            <FriendCard
+              // removeFriend={this.removeFriend}
+              id={friend.id}
+              key={friend.id}
+              image={friend.image}
+              handleClick={this.handleClick}
+              handleShuffle={this.handleShuffle}
+            />
+          ))}
+        </div>
       </Wrapper>
     );
   }
--- src/components/Title/Title.js
+++ src/components/Title/Title.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Title.css";
 
-const Title = props => <h1 className="title">{props.children}</h1>;
+const Title = props => 
+<div className="col-12 col-md-7 bg-secondary"><h1 className="title">Current Score: {props.score}</h1></div>;
 
 export default Title;
